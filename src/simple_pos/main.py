4c71e50dc2297202d607fa190f37097e332ca452
Kodai.T <fvbcj53523@yahoo.co.jp>
2026-02-08 21:36:41 +0900

    font_change

--- src/simple_pos/main.py
+++ src/simple_pos/main.py
@@ -1,4 +1,7 @@
 from simple_pos.apps.launcher import main as launcher_main
 import tkinter as tk
+import tkinter.font as tkFont
 root = tk.Tk()
+default_font = tkFont.nametofont("TkDefaultFont")
+default_font.configure(size=12, weight='bold')
 launcher = launcher_main(root)
